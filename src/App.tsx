59f3622ada307dec659094091d3b07c07e109db9
catureba <caturebadev@gmail.com>
2023-02-04 14:09:29 -0300

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Header from "./components/header-component";
+import Home from "./components/home-component";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header-component/index.tsx
@@ -0,0 +1,14 @@
+
+function Header(){
+    return(
+        <div className="header">
+            <a href="/#"><h1><img src="https://www.canva.com/design/DAFZoIQClas/view" alt="logo" /></h1></a>
+            <ul>
+                <li>Home</li>
+                <li>Post</li>
+                <li>List</li>
+            </ul>
+        </div>
+    );
+}
+export default Header;
