cf16c5b1d4e612ad12c3493b4a302ebe4a64bd6b
catureba <caturebadev@gmail.com>
2023-02-05 23:11:38 -0300

    update header

---
+++ src/components/assets/inputMoreButton-component/inputMoreButton.tsx
@@ -0,0 +1,10 @@
+import styles from './inputMoreButton.module.css'
+function InputMoreButton() {
+    return (
+        <form action="" className={styles.InputMoreButtonComponent}>
+            <input type="text" name="findByName" id="findByName" placeholder='Search a moment' />
+            <button type="submit">Find!</button>
+        </form>
+    );
+}
+export default InputMoreButton;
--- src/components/header-component/header.tsx
+++ src/components/header-component/header.tsx
@@ -1,16 +1,32 @@
+import InputMoreButton from '../assets/inputMoreButton-component/inputMoreButton';
 import styles from './header.module.css'
 function Header() {
     return (
-        <div className={styles.headerConteiner}>
-            <div className="box">
-                <a href="/#"><h1><img src="./assets/logo.png" alt="logo" /></h1></a>
-                <ul>
-                    <li>Home</li>
-                    <li>Post</li>
-                    <li>List</li>
-                </ul>
+        <nav className={styles.headerConteiner}>
+            <div className={styles.box}>
+                <div className={styles.logo}>
+                    <a href="http://localhost:3000/">
+                        <img src="./assets/logo.png" alt="logo" />
+                    </a>
+                </div>
+                <div className={styles.menu}>
+                    <ul>
+                        <li>
+                            <a href="http://localhost:3000/">Home</a>
+                        </li>
+                        <li>
+                            <a href="http://localhost:3000/">List</a>
+                        </li>
+                        <li>
+                            <a href="http://localhost:3000/">Post</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className={styles.searchBox}>
+                    <InputMoreButton />
+                </div>
             </div>
-        </div>
+        </nav>
     );
 }
 export default Header;
